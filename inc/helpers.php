0ee6e9ae0d4e4d7a9f2773277c705929f88b4d1f
Cody Marx Bailey <superphly@gmail.com>
2015-10-16 01:25:13 -0500

    no message

--- inc/helpers.php
+++ inc/helpers.php
@@ -5,3 +5,14 @@
 // Helper functions for random calls here and there
 //
 //************************************************************************
+
+// Quick shortcut to get the right module.
+
+function get_module_by_slug($page_slug) {
+	$module = get_page_by_path($page_slug, OBJECT, 'module');
+	if ($module) {
+		return $module;
+	} else {
+		return null;
+	}
+}
