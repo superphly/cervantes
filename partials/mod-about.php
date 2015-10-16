2edb55e2bd2deb5e823943b295a857422cbfd019
Cody Marx Bailey <superphly@gmail.com>
2015-10-16 02:02:23 -0500

    no message

---
+++ partials/mod-about.php
@@ -0,0 +1,14 @@
+<?php $module = get_module_by_slug("about"); ?>
+
+<div id="module-about" class="module">
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12 col-md-6">
+				image
+			</div>
+			<div class="col-xs-12 col-md-6">		
+				<?php the_field("text", $module->ID); ?>
+			</div>
+		</div>
+	</div>
+</div>
