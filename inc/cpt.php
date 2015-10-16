4b30b9b27f709d0a27c513b174d0f07462b28be9
Cody Marx Bailey <superphly@gmail.com>
2015-10-16 00:10:22 -0500

    no message

--- inc/cpt.php
+++ inc/cpt.php
@@ -50,3 +50,54 @@ function cpt_example() {
 
 // Hook into the 'init' action
 //add_action( 'init', 'cpt_example', 0 );
+
+// ***********************************************************************
+//
+// Modules CPT
+//
+// *********************************************************************** 
+
+function cpt_module() {
+
+	$labels = array(
+		'name'                => _x( 'Modules', 'Post Type General Name', 'text_domain' ),
+		'singular_name'       => _x( 'Module', 'Post Type Singular Name', 'text_domain' ),
+		'menu_name'           => __( 'Modules', 'text_domain' ),
+		'name_admin_bar'      => __( 'Modules', 'text_domain' ),
+		'parent_item_colon'   => __( 'Parent Item:', 'text_domain' ),
+		'all_items'           => __( 'All Items', 'text_domain' ),
+		'add_new_item'        => __( 'Add New Item', 'text_domain' ),
+		'add_new'             => __( 'Add New', 'text_domain' ),
+		'new_item'            => __( 'New Item', 'text_domain' ),
+		'edit_item'           => __( 'Edit Item', 'text_domain' ),
+		'update_item'         => __( 'Update Item', 'text_domain' ),
+		'view_item'           => __( 'View Item', 'text_domain' ),
+		'search_items'        => __( 'Search Item', 'text_domain' ),
+		'not_found'           => __( 'Not found', 'text_domain' ),
+		'not_found_in_trash'  => __( 'Not found in Trash', 'text_domain' ),
+	);
+	$args = array(
+		'label'               => __( 'module', 'text_domain' ),
+		'description'         => __( 'Page Modules', 'text_domain' ),
+		'labels'              => $labels,
+		'supports'            => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'page-attributes' ),
+		'hierarchical'        => false,
+		'public'              => false,
+		'show_ui'             => true,
+		'show_in_menu'        => true,
+		'menu_position'       => 3,
+		'menu_icon'           => 'dashicons-exerpt-view',
+		'show_in_admin_bar'   => false,
+		'show_in_nav_menus'   => false,
+		'can_export'          => true,
+		'has_archive'         => false,
+		'exclude_from_search' => true,
+		'publicly_queryable'  => false,
+		'capability_type'     => 'page',
+	);
+	register_post_type( 'module', $args );
+
+}
+
+// Hook into the 'init' action
+add_action( 'init', 'cpt_module', 0 );
