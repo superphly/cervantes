82fe3ea842b6f099b6c4e50a6e0d48c4771af4c6
Cody Marx Bailey <superphly@gmail.com>
2015-10-30 10:40:04 -0500

    no message

---
+++ partials/mod-contact.php
@@ -0,0 +1,14 @@
+<?php $module = get_module_by_slug("contact"); ?>
+
+<div id="module-contact" class="module">
+	<div class="container">
+		<div class="row">
+			<div class="col-xs-12 col-md-6">
+				<?php the_field("left_col", $module->ID); ?>
+			</div>
+			<div class="col-xs-12 col-md-6">
+				<?php the_field("right_col", $module->ID); ?>
+			</div>
+		</div>
+	</div>
+</div>
