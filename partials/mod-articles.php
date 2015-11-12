78800bdf7ae18c839206908a4e4a68d0def7da71
Cody Marx Bailey <superphly@gmail.com>
2015-11-12 13:48:41 -0600

    no message

--- partials/mod-articles.php
+++ partials/mod-articles.php
@@ -4,7 +4,7 @@
 	<div class="container">
 		<div class="row">
 
-			<div class="col-xs-12 col-md-3col-md-offset-2">
+			<div class="col-xs-12 col-md-3 col-md-offset-2">
 				<?php $image = get_field('image', $module->ID); ?>
 					
 				<?php if( !empty($image) ): ?>
