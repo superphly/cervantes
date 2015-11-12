0137d5431aa2d842bc70e56a2a86219dc9fc30bc
Cody Marx Bailey <superphly@gmail.com>
2015-11-12 13:40:59 -0600

    no message

--- partials/mod-contact.php
+++ partials/mod-contact.php
@@ -6,7 +6,7 @@
 			<div class="col-xs-12 col-md-6 col-md-offset-2">
 				<?php the_field("left_col", $module->ID); ?>
 			</div>
-			<div class="col-xs-12 col-md-2">
+			<div class="col-xs-12 col-md-2 text-large">
 				<?php the_field("right_col", $module->ID); ?>
 			</div>
 		</div>
