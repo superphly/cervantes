6c117001a7bb61f4cf109d06fbd64c329831eca2
Cody Marx Bailey <superphly@gmail.com>
2015-11-12 13:40:18 -0600

    no message

--- partials/mod-contact.php
+++ partials/mod-contact.php
@@ -3,10 +3,10 @@
 <div id="module-contact" class="module">
 	<div class="container">
 		<div class="row">
-			<div class="col-xs-12 col-md-4 col-md-offset-2">
+			<div class="col-xs-12 col-md-6 col-md-offset-2">
 				<?php the_field("left_col", $module->ID); ?>
 			</div>
-			<div class="col-xs-12 col-md-4">
+			<div class="col-xs-12 col-md-2">
 				<?php the_field("right_col", $module->ID); ?>
 			</div>
 		</div>
