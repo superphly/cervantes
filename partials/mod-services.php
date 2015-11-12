8c2a640b2a8ad4f563714c7233ebe5636b82af0d
Cody Marx Bailey <superphly@gmail.com>
2015-11-12 14:24:28 -0600

    no message

--- partials/mod-services.php
+++ partials/mod-services.php
@@ -3,7 +3,7 @@
 <div id="module-services" class="module">
 	<div class="container">
 		<div class="row">
-			<div class="col-xs-12">
+			<div class="col-xs-12 text-center">
 				<h2>Services</h2>
 			</div>
 			<div class="col-xs-12 col-md-4 col-md-offset-2">
