c24dc48d316c68aa0fafb6acf8bc5f06e51ef686
Cody Marx Bailey <superphly@gmail.com>
2015-11-12 13:47:36 -0600

    no message

--- partials/mod-articles.php
+++ partials/mod-articles.php
@@ -4,14 +4,14 @@
 	<div class="container">
 		<div class="row">
 
-			<div class="col-xs-12 col-md-3 col-md-offset-3">
+			<div class="col-xs-12 col-md-3col-md-offset-2">
 				<?php $image = get_field('image', $module->ID); ?>
 					
 				<?php if( !empty($image) ): ?>
 					<img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" class="img-responsive img-circle" />
 				<?php endif; ?>
 			</div>
-			<div class="col-xs-12 col-md-3">
+			<div class="col-xs-12 col-md-4">
 				<h2><?php the_field("header", $module->ID); ?></h2>
 				<?php the_field("text", $module->ID); ?>
 			</div>
