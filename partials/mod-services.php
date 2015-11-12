3a23002033e3c218d0eeb54ef7c27416e4e98e0a
Cody Marx Bailey <superphly@gmail.com>
2015-11-12 12:57:09 -0600

    no message

--- partials/mod-services.php
+++ partials/mod-services.php
@@ -3,10 +3,10 @@
 <div id="module-services" class="module">
 	<div class="container">
 		<div class="row">
-			<div class="col-xs-12 col-md-6">
+			<div class="col-xs-12 col-md-4 col-md-offset-2">
 				<?php the_field("left_col", $module->ID); ?>
 			</div>
-			<div class="col-xs-12 col-md-6">
+			<div class="col-xs-12 col-md-4">
 				<?php the_field("right_col", $module->ID); ?>
 			</div>
 		</div>
