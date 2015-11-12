e99080cffa2167fa0ea0509048e811736de7d156
Cody Marx Bailey <superphly@gmail.com>
2015-11-12 13:00:24 -0600

    no message

--- partials/mod-about.php
+++ partials/mod-about.php
@@ -3,14 +3,14 @@
 <div id="module-about" class="module">
 	<div class="container">
 		<div class="row">
-			<div class="col-xs-12 col-md-6">
+			<div class="col-xs-12 col-md-2 col-md-offset-2">
 				<?php $image = get_field('image', $module->ID); ?>
 					
 				<?php if( !empty($image) ): ?>
 					<img src="<?php echo $image['url']; ?>" alt="<?php echo $image['alt']; ?>" class="img-responsive" />
 				<?php endif; ?>
 			</div>
-			<div class="col-xs-12 col-md-6">		
+			<div class="col-xs-12 col-md-4">		
 				<?php the_field("text", $module->ID); ?>
 			</div>
 		</div>
