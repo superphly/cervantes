c95096d14522f0fc8fd0589b9f2dfbf5f111fdad
Cody Marx Bailey <superphly@gmail.com>
2015-11-17 11:42:47 -0600

    no message

--- partials/mod-cta.php
+++ partials/mod-cta.php
@@ -3,7 +3,7 @@
 <div id="module-cta" class="module">
 	<div class="container">
 		<div class="row">
-			<div class="col-xs-12 col-md-6 col-md-offset-3 text-center">
+			<div class="col-xs-12 col-md-12 text-center">
 				<h1><?php the_field("header", $module->ID); ?></h1>
 			</div>
 			<div class="col-xs-12 col-md-4 col-md-offset-4">
