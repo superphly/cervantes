5ec0b2f4eee1d0d91bebcdd8d34b9d569cc06ccc
Cody Marx Bailey <superphly@gmail.com>
2015-11-17 17:10:38 -0600

    no message

--- partials/mod-splash.php
+++ partials/mod-splash.php
@@ -5,6 +5,7 @@
 		<div class="row">
 			<div class="col-xs-12 text-center">
 				<h1><?php echo get_bloginfo('sitename'); ?></h1>
+				<h3><?php bloginfo('description');?></h3>
 				<div class="vid-container">
 					<?php the_field("youtube", $module->ID); ?>
 				</div>
