12838e3d14ed29ecfdb885f037d36b5f4488c3dc
Cody Marx Bailey <superphly@gmail.com>
2015-11-17 11:33:02 -0600

    no message

--- inc/fonts.php
+++ inc/fonts.php
@@ -1,7 +1,7 @@
 <?php 
 
 function cerv_fonts() {
-	wp_enqueue_style('hind', 'http://fonts.googleapis.com/css?family=Hind:700,400,300');
+	wp_enqueue_style('lato', 'https://fonts.googleapis.com/css?family=Lato');
 	wp_enqueue_style('opensans', 'http://fonts.googleapis.com/css?family=Open+Sans:400,700,300');
 }
 
