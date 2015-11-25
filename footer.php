1debc4b9cc2299f34ae68aacbcf45760909d4f03
Cody Marx Bailey <superphly@gmail.com>
2015-11-25 16:45:27 -0600

    no message

--- footer.php
+++ footer.php
@@ -87,5 +87,21 @@
             ga('create', 'xxx', 'auto');
             ga('send', 'pageview');
         </script>
+        <script>
+            jQuery(function($) {
+              $('a[href*=#]:not([href=#])').click(function() {
+                if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+                  var target = $(this.hash);
+                  target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+                  if (target.length) {
+                    $('html,body').animate({
+                      scrollTop: target.offset().top
+                    }, 1000);
+                    return false;
+                  }
+                }
+              });
+            });
+        </script>
     </body>
 </html>
